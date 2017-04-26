6f8d5532d7f4359e02e001eb7164babe63720fd5
jorian <jorian@outlook.com>
2017-04-26 11:59:01 +0200

    Added test files

---
+++ src/test/controller/AddTimelineTest.java
@@ -0,0 +1,14 @@
+package test.controller;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by JorianWielink on 26/04/2017.
+ */
+public class AddTimelineTest {
+    @org.junit.Test
+    public void displayBtn() throws Exception {
+
+    }
+
+}
---
+++ src/test/model/TimelineTest.java
@@ -0,0 +1,10 @@
+package test.model;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by JorianWielink on 26/04/2017.
+ */
+public class TimelineTest {
+
+}
