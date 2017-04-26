dd41ea7f9f991a5b9b1d64c30bb991875a14b856
jorian <jorian@outlook.com>
2017-04-26 16:07:12 +0200

    Added timeline model

---
+++ src/main/model/Timeline.java
@@ -0,0 +1,55 @@
+package main.model;
+
+import java.time.LocalDate;
+
+public class Timeline {
+	// Creating timeline with its components
+	private String title;
+	private String description;
+	private LocalDate startDate, endDate;
+
+	public Timeline(String t, String des, LocalDate stDate, LocalDate endDate) {
+
+		this.title = t;
+		this.description = des;
+		this.startDate = stDate;
+		this.endDate = endDate;
+
+	}
+
+	// setters and getter of the timeline components
+
+	public void setTitle(String t) {
+		title = t;
+	}
+
+	public void setDesctription(String d) {
+		description = d;
+	}
+
+	public void setStartDate(LocalDate sD) {
+		startDate = sD;
+	}
+
+	public void setEndDate(LocalDate eD) {
+		endDate = eD;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public LocalDate getStartDate() {
+		return startDate;
+
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+}
