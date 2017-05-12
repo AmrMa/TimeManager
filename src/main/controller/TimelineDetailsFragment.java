7f321d7c4659393aa8c3af695a4bd4237cf4d564
AmrMa <amr.hasan.mahmoud@gmail.com>
2017-05-12 18:18:54 +0200

    Add files via upload
    multiple events, remove timeline, add an image to a timeline, and fixing the appearance of the timeline object.

--- src/main/controller/TimelineDetailsFragment.java
+++ src/main/controller/TimelineDetailsFragment.java
@@ -2,7 +2,6 @@ package main.controller;
 
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Cursor;
 import javafx.scene.control.Button;
@@ -17,7 +16,6 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
@@ -32,11 +30,10 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.imageio.ImageIO;
-
-import com.sun.glass.ui.Window.Level;
-import com.sun.javafx.logging.Logger;
 
 import static main.common.StageManager.getStage;
 import static main.controller.NewEventFragment.myEvent;
@@ -47,73 +44,100 @@ public class TimelineDetailsFragment {
     @FXML private Button ButtonBack;
     @FXML private AnchorPane myDisplay;
     @FXML private Button newEventButton;
-    @FXML private ImageView imageView;
-    @FXML private Button addImage;
-    @FXML private Button edittimeline;
-    @FXML
-    private Button removetimeline;
-    @FXML private Text EndTime;
-    @FXML private Text StartTime;
-    @FXML private Text Description;
+    @FXML private Text title;
+    @FXML private Label EndDate;
+    @FXML private Label startDate;
+    @FXML private Label Description;
+    @FXML private ImageView timeline_image;
+    @FXML private Button RemoveTimeline;
+    @FXML private Button AddImage;
+    @FXML private AnchorPane LeftPane;
     Timeline display = myTime;
     double lineHeight;
- 
-   @FXML Text title;
-   @FXML private AnchorPane Event_Viewer;
+    Period timelinePeriod;
+    private Line lineTimeline;
+    ArrayList<LocalDate> duplicates = new ArrayList<LocalDate>();
     public void initialize() throws SQLException {
         ButtonBack.setOnMouseEntered(e -> getStage().getScene().setCursor(Cursor.HAND));
         ButtonBack.setOnMouseExited(e -> getStage().getScene().setCursor(Cursor.DEFAULT));
-        lineHeight = myDisplay.getLayoutY() / 2;       
+        lineHeight = myDisplay.getLayoutY() / 2;
+        timelinePeriod = display.getStartDate().until(display.getEndDate());
         title.setText(display.getTitle());
-        StartTime.setText(display.getStartDate().toString());
-        EndTime.setText(display.getEndDate().toString());
+        EndDate.setText(display.getEndDate().toString());
+        startDate.setText(display.getStartDate().toString());
         Description.setText(display.getDescription());
+        displayTimeline(timelinePeriod.getDays());
         displayEvents();
 
     }
 
+    private void displayTimeline(int period) {
+        lineTimeline = new Line(0,lineHeight,myDisplay.getPrefWidth(),lineHeight);
+        myDisplay.getChildren().add(lineTimeline);
+        double distanceBetweenLines = myDisplay.getPrefWidth() / period;
+        for (int i = 1; i < timelinePeriod.getDays(); i++) {
+            Line verticalLine = new Line(i * distanceBetweenLines,lineHeight-5,i * distanceBetweenLines, lineHeight+5);
+            myDisplay.getChildren().add(verticalLine);
+        }
+    }
 
     private void displayEvents() {
         ArrayList<Event> events = myTime.getListOfEvents();
-        int i=10;
+
+        /**
+         * For each event, I try to calculate the position of the event on the timeline.
+         * I do this by getting the total length of the line, being the total period in days of the timeline.
+         * I compare this with the days until the event takes place, given the date of the event and the start date of
+         * the timeline.
+         *
+         * Dividing the days until the event with the total days of the timeline, I get the relative position of where to
+         * put the event. This position is then used by creating a line on the timeline.
+         *
+         */
         for (Event e: events) {
-        VBox eventing = new VBox(5);
-        Label titleOfEvent = new Label(e.getEvent_title());
-        Button Remov = new Button("-");                   // Remove an event
-        Remov.setOnAction(new EventHandler<ActionEvent>() {
-            @Override public void handle(ActionEvent e) {
-               eventing.getChildren().clear();
-            }
-        });
-        ImageView imgv =new ImageView();
-        Button addimg = new Button("add image");
-        imgv.setFitHeight(30);
-        imgv.setFitWidth(30);
-        addimg.setOnAction(new EventHandler<ActionEvent>() {
-            @Override public void handle(ActionEvent e) {
-            	 FileChooser fileChooser = new FileChooser();
-                 
-                 //Set extension filter
-                 FileChooser.ExtensionFilter extFilterJPG = new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.JPG");
-                 FileChooser.ExtensionFilter extFilterPNG = new FileChooser.ExtensionFilter("PNG files (*.png)", "*.PNG");
-                 fileChooser.getExtensionFilters().addAll(extFilterJPG, extFilterPNG);
-                   
-                 //Show open file dialog
-                 File file = fileChooser.showOpenDialog(null);
-                            
-                 try {
-                     BufferedImage bufferedImage = ImageIO.read(file);
-                     Image image = SwingFXUtils.toFXImage(bufferedImage, null);
-                     imgv.setImage(image);
-                 } catch (IOException ex) {
-                  
-                 }
-             }
-         });
-        eventing.getChildren().addAll(titleOfEvent,Remov,imgv,addimg);
-        eventing.setLayoutX(i);
-        Event_Viewer.getChildren().add(eventing);
-        i=i+100;
+        	VBox vbox = new VBox();
+            LocalDate eventMoment = e.getEvent_startDate();
+            Period periodUntilEvent = display.getStartDate().until(eventMoment);
+
+            int totalDays = timelinePeriod.getDays();
+            int daysUntilEvent = periodUntilEvent.getDays();
+            System.out.println("Totaldays: " + totalDays);
+            System.out.println("Days until event: " + daysUntilEvent);
+
+            int relativePosition = (daysUntilEvent * 100) / totalDays;
+
+            double positionToPutEvent = myDisplay.getPrefWidth() * relativePosition / 100;
+            System.out.println("Position to put event: " + positionToPutEvent);
+            Circle eventCircle = new Circle(10, Color.WHITE);
+            eventCircle.setStrokeWidth(1.0);
+            eventCircle.setStroke(Color.BLACK);
+            eventCircle.setOnMouseEntered(event -> getStage().getScene().setCursor(Cursor.HAND));
+            eventCircle.setOnMouseExited(event -> getStage().getScene().setCursor(Cursor.DEFAULT));
+            eventCircle.setOnMouseClicked(event -> {
+                myEvent = e;
+                try {
+                    ScreenController.setScreen(ScreenController.Screen.NEW_EVENT);
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            });
+            Label dateOfEvent = new Label(e.getEvent_startDate().toString());
+            Label titleOfEvent = new Label(e.getEvent_title());
+            titleOfEvent.setFont(Font.font(15));
+            vbox.getChildren().addAll(eventCircle,titleOfEvent,dateOfEvent);        
+            duplicates.add(e.getEvent_startDate());
+            int y=100;
+           if(duplicates(duplicates)){
+        	   vbox.relocate(positionToPutEvent - 5,lineHeight-y);
+        	   y=y+10;
+        	   duplicates.clear();
+           }
+           else
+            vbox.relocate(positionToPutEvent - 5,lineHeight-10);
+            
+          
+            myDisplay.getChildren().add(vbox);
+           
         }
     }
 
@@ -128,6 +152,17 @@ public class TimelineDetailsFragment {
     @FXML
     public void addEvent() throws IOException {
         ScreenController.setScreen(ScreenController.Screen.NEW_EVENT);
+    }    
+   
+    boolean duplicates(final ArrayList<LocalDate> zipcodelist)
+    {
+      Set<LocalDate> lump = new HashSet<LocalDate>();
+      for (LocalDate i : zipcodelist)
+      {
+        if (lump.contains(i)) return true;
+        lump.add(i);
+      }
+      return false;
     }
     @FXML
     void Addimage(ActionEvent event) {
@@ -144,19 +179,15 @@ public class TimelineDetailsFragment {
          try {
              BufferedImage bufferedImage = ImageIO.read(file);
              Image image = SwingFXUtils.toFXImage(bufferedImage, null);
-             imageView.setImage(image);
+             timeline_image.setImage(image);
          } catch (IOException ex) {
           
          }
     }
     @FXML
     void Removetimeline(ActionEvent event) throws IOException{
-   //   myDisplay.getChildren().removeAll(imageView,lineTimeline);
     	myDisplay.getChildren().clear();
-    }
-    @FXML
-    void edittimeline(ActionEvent event) throws IOException{
-   
-    	myDisplay.getChildren().clear();
+    	LeftPane.getChildren().clear();
+    	timeline_image.setImage(null);
     }
 }
