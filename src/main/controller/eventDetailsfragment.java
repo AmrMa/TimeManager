d0fdb67471e037a78b177c74b35b0781345719ff
AmrMa <amr.hasan.mahmoud@gmail.com>
2017-05-13 09:31:47 +0200

    Add files via upload
    Added image to event

--- src/main/controller/eventDetailsfragment.java
+++ src/main/controller/eventDetailsfragment.java
@@ -1,9 +1,20 @@
 package main.controller;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import main.common.ScreenController;
 
 public class eventDetailsfragment {
 
@@ -14,18 +25,34 @@ public class eventDetailsfragment {
     private AnchorPane PaneMain;
 
     @FXML
-    private Button newEventButton;
-
+    private Button Addimage;
+    @FXML ImageView imageview;
    
 
     @FXML
-    void back(ActionEvent event) {
-
+    void back(ActionEvent event) throws IOException {
+    	ScreenController.setScreen(ScreenController.Screen.TIMELINE_DETAILS);
     }
 
     @FXML
     void addEvent(ActionEvent event) {
-
+ FileChooser fileChooser = new FileChooser();
+         
+         //Set extension filter
+         FileChooser.ExtensionFilter extFilterJPG = new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.JPG");
+         FileChooser.ExtensionFilter extFilterPNG = new FileChooser.ExtensionFilter("PNG files (*.png)", "*.PNG");
+         fileChooser.getExtensionFilters().addAll(extFilterJPG, extFilterPNG);
+           
+         //Show open file dialog
+         File file = fileChooser.showOpenDialog(null);
+                    
+         try {
+             BufferedImage bufferedImage = ImageIO.read(file);
+             Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+             imageview.setImage(image);
+         } catch (IOException ex) {
+          
+         }
     }
 
 }
